3db5a10a3202297c3c976bf2a7b3002259a46c65
DavitBoo <davidmarras@gmail.com>
2023-03-15 15:31:48 +0100

    ProfesionalInfo.js from class to function

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,111 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class PersonalInfo extends Component {
-    constructor(props) {
-      super(props);
 
-      this.state = {
-        name: '',
-        title: '',
-        email: '',
-        phoneNumber: '',
-        birthDate: '',
-        description: '',
-        pictureUrl: ''
+export default function PersonalInfo(props) {
+  const [pictureUrl, setPictureUrl] = useState('')
+
+  const nameChange = (e) => {
+    const { name, value, files } = e.target
+
+    if (name === 'picture'){
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPictureUrl(reader.result)
+        props.onFormChanges(reader.result, name);
       };
-
-      this.nameChange = this.nameChange.bind(this);
+      reader.readAsDataURL(files[0]); 
     }
-
-    nameChange(e){
-      const { name, value, files } = e.target
-
-      if (name === 'picture'){
-        const reader = new FileReader();
-        reader.onload = () => {
-          this.setState({ pictureUrl: reader.result });
-          this.props.onFormChanges(reader.result, name);
-        };
-        reader.readAsDataURL(files[0]); 
-      }
-      else this.props.onFormChanges(value, name);
-    }
-
-
-    render() {
-      
-      return (
-
-        <div id="personal-info" className='flex-column'>
-            <h2>Personal Information</h2>
-            <div className="contact-info flex">
-                <label htmlFor="name">Name:
-                    <input 
-                      onChange={this.nameChange}
-                      type="name" 
-                      id="name" 
-                      name="fullName"
-                    />
-                </label>
-
-                <label htmlFor="title">Title:
-                    <input 
-                      onChange={this.nameChange}
-                      type="" 
-                      id="title" 
-                      name="title"
-                    />
-                </label>
-            </div>
-
-            <div className="flex">
-                <label htmlFor="email">E-mail:
-                    <input 
-                      onChange={this.nameChange}
-                      type="email" 
-                      id="email" 
-                      name="email"
-                    />
-                </label>
-                <label htmlFor="tel">Phone number:
-                    <input 
-                      onChange={this.nameChange}
-                      type="tel" 
-                      id="tel" 
-                      name="telephone"
-                    />
-                </label>
-              <label htmlFor="birthdate">Birth date
-                  <input 
-                    onChange={this.nameChange}
-                    type="date" 
-                    name="birthdate" 
-                    id="birthdate" 
-                  />
-              </label>
-              <label htmlFor=""></label>
-            </div>
-            <label htmlFor="picture">Insert profil picture:
-              <input 
-                onChange={this.nameChange}
-                type="file" 
-                name="picture" 
-                id="picture" 
-                accept="image/*"
-              />
-            </label>
-            <div className="flex-column">
-                <label htmlFor="description">Description:</label>
-                <textarea 
-                  onChange={this.nameChange}
-                  name="description" 
-                  id="description" 
-                ></textarea>
-                
-            </div>
-        </div>
-      );
-    }
+    else props.onFormChanges(value, name);
   }
-  
-  export default PersonalInfo;
+
+  return (
+
+    <div id="personal-info" className='flex-column'>
+        <h2>Personal Information</h2>
+        <div className="contact-info flex">
+            <label htmlFor="name">Name:
+                <input 
+                  onChange={nameChange}
+                  type="name" 
+                  id="name" 
+                  name="fullName"
+                />
+            </label>
+
+            <label htmlFor="title">Title:
+                <input 
+                  onChange={nameChange}
+                  type="" 
+                  id="title" 
+                  name="title"
+                />
+            </label>
+        </div>
+
+        <div className="flex">
+            <label htmlFor="email">E-mail:
+                <input 
+                  onChange={nameChange}
+                  type="email" 
+                  id="email" 
+                  name="email"
+                />
+            </label>
+            <label htmlFor="tel">Phone number:
+                <input 
+                  onChange={nameChange}
+                  type="tel" 
+                  id="tel" 
+                  name="telephone"
+                />
+            </label>
+          <label htmlFor="birthdate">Birth date
+              <input 
+                onChange={nameChange}
+                type="date" 
+                name="birthdate" 
+                id="birthdate" 
+              />
+          </label>
+          <label htmlFor=""></label>
+        </div>
+        <label htmlFor="picture">Insert profil picture:
+          <input 
+            onChange={nameChange}
+            type="file" 
+            name="picture" 
+            id="picture" 
+            accept="image/*"
+          />
+        </label>
+        <div className="flex-column">
+            <label htmlFor="description">Description:</label>
+            <textarea 
+              onChange={nameChange}
+              name="description" 
+              id="description" 
+            ></textarea>
+            
+        </div>
+    </div>
+  );
+}
